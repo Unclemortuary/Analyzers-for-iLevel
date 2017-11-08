a2ca7937f4bffb1356b528c1af267bf1a913377b
Aleksandr Kugushev <aleksandr_kugushev@epam.com>
2017-11-08 19:20:27 +0300

    move current verstion fo BestPracticies library from ilevel repository

---
+++ CodeAnalysis/iLevel.CodeAnalysis.BestPractices/ArgumentTrailingUnderscoreAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis;
+
+namespace iLevel.ViewPoint.CodeAnalysis.BestPractices
+{
+
+    /// <summary>
+    /// Analyzer argument names that end on underscore
+    /// void Method(Object arg_)
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ArgumentTrailingUnderscoreAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "ILVL0001";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, "Ugly argument name", "Rename argument name", "iLevel.BestPractices", DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(Do, SyntaxKind.Parameter);
+        }
+
+        private void Do(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is ParameterSyntax parameter &&
+                parameter.Identifier.Text.EndsWith("_"))
+            {
+                var diagnostic = Diagnostic.Create(Rule, parameter.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ CodeAnalysis/iLevel.CodeAnalysis.BestPractices/ArgumentTrailingUnderscoreFixProvider.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using System.Composition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CodeActions;
+using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Rename;
+
+namespace iLevel.ViewPoint.CodeAnalysis.BestPractices
+{
+    /// <summary>
+    /// Fix methods that have argument names that end on underscore
+    /// void Method(Object arg_) => void Method(Object arg)
+    /// </summary>
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ArgumentTrailingUnderscoreFixProvider)), Shared]
+    public class ArgumentTrailingUnderscoreFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(ArgumentTrailingUnderscoreAnalyzer.DiagnosticId);
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            Diagnostic diagnostic = context.Diagnostics.Single();
+
+            var node = root.FindNode(diagnostic.Location.SourceSpan);
+
+            CodeAction codeAction = CodeAction.Create("Rename",
+                createChangedSolution: t => RemoveUglyTail(context.Document, node, t));
+
+            context.RegisterCodeFix(codeAction, diagnostic);
+        }
+
+        private async Task<Solution> RemoveUglyTail(Document document, SyntaxNode node, CancellationToken token)
+        {
+
+            if (node is ParameterSyntax parameter)
+            {
+                string old = parameter.Identifier.Text;
+                string fxd = old.TrimEnd('_');
+
+                var model = await document.GetSemanticModelAsync(token).ConfigureAwait(false);
+                IParameterSymbol symbol = model.GetDeclaredSymbol(parameter, token);
+                
+                return await Renamer.RenameSymbolAsync(document.Project.Solution, symbol, fxd, document.Project.Solution.Options, token);
+            }
+            else
+            {
+                throw new InvalidOperationException($"{node.GetType()}: {node.GetText()}");
+            }
+        }
+    }
+}
