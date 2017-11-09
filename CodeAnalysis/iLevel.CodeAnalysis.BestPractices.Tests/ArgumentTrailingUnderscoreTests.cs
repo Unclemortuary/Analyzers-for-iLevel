57e438700232846fc25b188dcd865fbcab571852
Kugushev Aleksandr <kugushew@gmail.com>
2017-11-09 22:54:40 +0300

    add tests for ArgumentTrailingUnderscore

---
+++ CodeAnalysis/iLevel.CodeAnalysis.BestPractices.Tests/ArgumentTrailingUnderscoreTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using iLevel.CodeAnalysis.BestPractices.Tests.Common;
+using Microsoft.CodeAnalysis.Diagnostics;
+using iLevel.ViewPoint.CodeAnalysis.BestPractices;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis;
+
+namespace iLevel.CodeAnalysis.BestPractices.Tests
+{
+    [TestClass]
+    public class ArgumentTrailingUnderscoreTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void EmptyMethod_ShowWarning()
+        {
+           const string test = @"
+class Class
+{
+    public void Method(string arg_) { }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = "ILVL0001",
+                Message = "Rename argument name",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 4, 24) }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+            const string fixtest = @"
+class Class
+{
+    public void Method(string arg) { }
+}";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [TestMethod]
+        public void EmptyMethod_FixArgument()
+        {
+            const string test = @"
+class Class
+{
+    public void Method(string arg_) { }
+}";
+            const string fixtest = @"
+class Class
+{
+    public void Method(string arg) { }
+}";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [TestMethod]
+        public void NotEmptyMethod_FixArgumentAndUsages()
+        {
+            const string test = @"
+class Class
+{
+    public void Method(string arg_) 
+    {
+        string nothing = arg_.Trim();
+    }
+}";
+            const string fixtest = @"
+class Class
+{
+    public void Method(string arg) 
+    {
+        string nothing = arg.Trim();
+    }
+}";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+
+        [TestMethod]
+        public void MethodLambda_FixArgumentAndUsages()
+        {
+            const string test = @"
+class Class
+{
+    public void Method(string arg_) => arg_.Trim();
+}";
+            const string fixtest = @"
+class Class
+{
+    public void Method(string arg) => arg.Trim();
+}";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [TestMethod]
+        public void Interface_FixArgument()
+        {
+            const string test = @"
+interface Interface
+{
+    void Method(string arg_);
+}";
+            const string fixtest = @"
+interface Interface
+{
+    void Method(string arg);
+}";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [TestMethod]
+        public void NamedArgumentExecution_FixArgument()
+        {
+            const string test = @"
+class Class
+{
+   public static void Method(string arg_) { }
+}
+
+class Executor
+{
+    public void Execute() => Class.Method(arg_: ""Hello World!!!"");
+}";
+            const string fixtest = @"
+class Class
+{
+   public static void Method(string arg) { }
+}
+
+class Executor
+{
+    public void Execute() => Class.Method(arg: ""Hello World!!!"");
+}";
+            VerifyCSharpFix(test, fixtest);
+        }
+       
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+            => new ArgumentTrailingUnderscoreAnalyzer();
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+            => new ArgumentTrailingUnderscoreFixProvider();
+    }
+}
---
+++ CodeAnalysis/iLevel.CodeAnalysis.BestPractices.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("iLevel.CodeAnalysis.BestPractices.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("iLevel.CodeAnalysis.BestPractices.Tests")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d44fcf1-0e9e-4970-84e2-664d3d4e51ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
