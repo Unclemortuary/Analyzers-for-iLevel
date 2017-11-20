dbae049fd2a9234499a6b5e942f04a2fff591a17
Unclemortuary <artem_mihalych@mail.ru>
2017-11-20 18:04:49 +0300

    MVC template added

--- CodeAnalysis/CodeAnalysisService/Global.asax.cs
+++ CodeAnalysis/CodeAnalysisService/Global.asax.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.Http;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace CodeAnalysisService
 {
@@ -12,6 +14,10 @@ namespace CodeAnalysisService
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
---
+++ CodeAnalysis/CodeAnalysisService/Models/DiagnosticError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeAnalysisService.Models
+{
+    public class DiagnosticError
+    {
+        public string FileName { get; set; }
+        public string WarningText { get; set; }
+
+    }
+}
