87b870a034793c846055748d850316958882eabe
Unclemortuary <artem_mihalych@mail.ru>
2017-12-14 20:45:37 +0300

    Microsoft.CodeAnalysis to 2.4; integration test for missed reference, bad test checking service in loop case

--- CodeAnalysis/iLevel.CodeAnalysis.ServiceIntegrationTests/HomeControllerIntegrationTests.cs
+++ CodeAnalysis/iLevel.CodeAnalysis.ServiceIntegrationTests/HomeControllerIntegrationTests.cs
@@ -50,8 +50,7 @@ namespace iLevel.CodeAnalysis.ServiceIntegrationTests
         {
 
         }
-    }
-";
+    }";
             _input.Add("Program", argumentUnderscoreTest);
             var controller = UnityConfig.Container.Resolve<HomeController>();
             controller.GetCompilationDiagnostic(_input);
@@ -76,7 +75,7 @@ class Program
         }
 
         [TestMethod]
-        public void FilesWithoutProgramMethod_GetCorrespondingMessage()
+        public void FilesWithoutProgramMethod_ReturnsCorrespondingMessage()
         {
             string argumentUnderscoreTest = @"
     class Program
@@ -102,7 +101,47 @@ class Program
         }
 
         [TestMethod]
-        public void FilesWithArgumentNamesUnderscore_GetDiagnosticFromILVL0001Analyzer()
+        public void FilesWithtMissedReference_ReturnsCorrespondingMessage()
+        {
+            string classWithServiceMethodDeclaration = @"
+namespace Test_classes_for_analyzer
+{
+    class Class1
+    {
+        public void someMethod(int someArg, int anotherArg) { }
+    }
+}";
+            string classUsesService = @"
+namespace SomeNamespace
+{
+    class Program
+    {
+        static void Main(string[] args___)
+        {
+             Class1 class1 = new Class1();
+             class1.someMethod(5);
+        }
+    }
+}";
+            _input.Add("Class", classWithServiceMethodDeclaration);
+            _input.Add("Program", classUsesService);
+
+            AnalyzerConfig.RegisterAnalyzers(AnalyzerProvider.Analyzers);
+
+            var controller = UnityConfig.Container.Resolve<HomeController>();
+            var result = (List<string>)controller.GetCompilationDiagnostic(_input).Data;
+            var expected = new ServiceDiagnosticResult()
+            {
+                Location = { FileName = "Program", Line = 8, Column = 14 },
+                SeveretyType = SeveretyType.error,
+                DiagnosticMessage = "The type or namespace name 'Class1' could not be found (are you missing a using directive or an assembly reference?)",
+                AnalyzerID = ""
+            };
+            ServiceDiagnosticVerifier.Verify(expected, result[0]);
+        }
+
+        [TestMethod]
+        public void FilesWithArgumentNamesUnderscore_ReturnsWarningFromILVL0001Analyzer()
         {
             string argumentUnderscoreTest = @"
     class Program
@@ -127,6 +166,49 @@ class Program
             ServiceDiagnosticVerifier.Verify(expected, result);
         }
 
-        
+        [TestMethod]
+        public void FileUsesServiceInLoop_ReturnsWarningFromILVL0002Analyzer()
+        {
+            string service = @"
+namespace Service
+{
+    public interface ISomeService
+    {
+        void Method();
+    }
+}";
+
+            string serviceInLoopTest = @"
+using Service;
+
+    class Program
+    {
+		static ISomeService service = null;
+		
+        static void Main(string[] args)
+        {
+            do
+			{
+				service.Method();
+			} while(true);
+        }
+    }";
+            _input.Add("Service", service);
+            _input.Add("Program", serviceInLoopTest);
+
+            AnalyzerConfig.RegisterAnalyzers(AnalyzerProvider.Analyzers);
+
+            var controller = UnityConfig.Container.Resolve<HomeController>();
+            var resultList = (List<string>)controller.GetCompilationDiagnostic(_input).Data;
+            var result = resultList[0];
+            var expected = new ServiceDiagnosticResult()
+            {
+                Location = { FileName = "Program", Line = 12, Column = 17 },
+                SeveretyType = SeveretyType.warning,
+                DiagnosticMessage = "Service method shouldn't be executed in a loop because there is no guarantee that cache has initialized properly",
+                AnalyzerID = "ILVL0002"
+            };
+            ServiceDiagnosticVerifier.Verify(expected, result);
+        }
     }
 }
