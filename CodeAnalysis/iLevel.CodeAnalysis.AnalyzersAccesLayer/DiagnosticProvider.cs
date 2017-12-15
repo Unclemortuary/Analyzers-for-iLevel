6722679ab0e7b7f863635b25a1347eb9f2e23264
Unclemortuary <artem_mihalych@mail.ru>
2017-12-15 12:59:39 +0300

    Created empty Analyzers Acces Layer

---
+++ CodeAnalysis/iLevel.CodeAnalysis.AnalyzersAccesLayer/DiagnosticProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iLevel.CodeAnalysis.AnalyzersAccesLayer
+{
+    public interface IDiagnosticProvider
+    {
+        Report GetAnalyzersDiagnostic();
+        Report GetCompilationDiagnostic();
+    }
+
+    class DiagnosticProvider : IDiagnosticProvider
+    {
+        public Report GetAnalyzersDiagnosticReport()
+        {
+
+        }
+
+        public Report GetCompilationDiagnostic()
+        {
+
+        }
+
+        private DiagnosticResult[] GetUnsortedDiagnostic()
+        {
+
+        }
+    }
+}
